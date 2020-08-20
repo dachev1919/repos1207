b972ab442ad32f1971e544ed50b592170b4ed1cc
dachev1919 <dachev1919@gmail.com>
2020-08-20 23:52:18 +0300

    some changes

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!doctype html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
-
-    <title>test</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Document</title>
 </head>
 <body>
-    
+
 </body>
 </html>
