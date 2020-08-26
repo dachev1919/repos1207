c0ef51ef76ff60d4461ce12864e5aa7575e8d8f4
dachev1919 <dachev1919@gmail.com>
2020-08-26 14:15:00 +0300

    Made some changes

--- index.php
+++ index.php
@@ -6,9 +6,40 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/normalize.css">
     <title>Document</title>
 </head>
 <body>
+<header class="header">
+    <div class="navbar">
+        <div class="container">
+            <button class="menu-button">
+                <span class="menu-button__line"></span>
+                <span class="menu-button__line"></span>
+                <span class="menu-button__line"></span>
+            </button>
+            <div class="logo navbar__logo">
+                <stong class="logo__text">Dachev Write It</stong>
+                <span class="logo__description">Индивидуальная разработка сайтов</span>
+            </div>
+            <div class="navbar__mobile">
+                <a class="navbar__phone" href="tell:+380636219861">+380636219861</a>
+                <span class="navbar__work-time">без выходных 9:00 - 21:00</span>
+            </div>
+            <div class="navbar__landline">
+                <a class="navbar__phone" href="tell:+380992290079">+380992290079</a>
+                <a class="navbar__phone" href="mailto:dachev1919@gmai.com">dachev1919@gmai.com</a>
+            </div>
+            <button class="button-o navbar__button">Получить консультацию</button>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.navbar -->
+    <div class="hero">
 
+    </div>
+    <!-- /.hero -->
+</header>
+<!-- /.header -->
 </body>
 </html>
